441cc7a746cbbfd65df0fce99df8d9e77b0e7203
larson cop <97100495@st.deltion.nl>
2023-03-22 14:44:53 +0100

    x

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 document.addEventListener("DOMContentLoaded", (e) => {
- 
+    let levens;
+    let guess;
+    let guesses;
+    let categories
+    let getHint;
+    let counter;
+    let space;
+    
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+  const Dieren = [[], [], [], [], [], [], [], [], [], []];
+  const Autos = [[], [], [], [], [], [], [], [], [], []];
+  const Films = [[], [], [], [], [], [], [], [], [], []];
+  const Landen = [[], [], [], [], [], [], [], [], [], []];
+});
